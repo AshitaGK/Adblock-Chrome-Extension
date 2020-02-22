c15bc7101c85e0fd3e4ded792966f49acc2d3fc2
AshitaGK <ashita.gk696@gmail.com>
2020-02-22 08:12:58 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,15 @@
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	
+	
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
